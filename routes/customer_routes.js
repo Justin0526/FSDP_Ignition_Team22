192f3cfd0757309959b75725832f2c88ce222176
SharliCain <s10269743@connect.np.edu.sg>
2025-11-09 11:40:43 +0800

    Cleaned old backend files before adding Next.js client

--- routes/customer_routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import * as customerController from "../controllers/customer_controller.js";
-
-const router = express.Router();
-router.get("/", customerController.getAllCustomers);
-
-export default router;
