bd4afee70fca799fe512aead7b5790e17a0c20ed
Justin Tang <jtang8446@gmail.com>
2025-11-22 16:28:30 +0800

    Restart

--- client/src/core/services/self_service.service.js
+++
@@ -1,7 +0,0 @@
-import * as repo from "@/core/repos/self_service.repo";
-
-export async function getSelfServiceByCategory(categoryId){
-    if(!categoryId) throw new Error("CATEGORY_REQUIRED");
-
-    return repo.getSelfServiceByCategory(categoryId);
-}
